c4422b724b3c2ac5591a3376d8bb9f6440f522c5
chillfont <chillfont@gmail.com>
2026-03-31 13:24:26 +0700

    8.5 Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from pydantic import BaseModel, EmailStr
 from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
+from clients.private_http_builder import AuthenticationUserSchema
+from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 
@@ -17,6 +19,10 @@ class UserFixture(BaseModel):
     def password(self) -> str:
         return self.request.password
 
+    @property
+    def authentication_user(self) -> AuthenticationUserSchema:
+        return AuthenticationUserSchema(email=self.email, password=self.password)
+
 
 @pytest.fixture
 def authentication_client() -> AuthenticationClient:
@@ -26,6 +32,11 @@ def authentication_client() -> AuthenticationClient:
 @pytest.fixture
 def public_users_client() -> PublicUsersClient:
     return get_public_users_client()
+
+
+@pytest.fixture
+def private_users_client(function_user: UserFixture) -> PrivateUsersClient:
+    return get_private_users_client(function_user.authentication_user)
 
 
 @pytest.fixture
