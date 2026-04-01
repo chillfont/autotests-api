cbe7a00dac51d1be0f7b75d866ac5f29d1ff92a2
chillfont <chillfont@gmail.com>
2026-04-01 09:04:46 +0700

    8.6 Плагины Pytest

--- fixtures/users.py
+++ fixtures/users.py
@@ -1,6 +1,5 @@
 import pytest
 from pydantic import BaseModel, EmailStr
-from clients.authentication.authentication_client import get_authentication_client, AuthenticationClient
 from clients.private_http_builder import AuthenticationUserSchema
 from clients.users.private_users_client import PrivateUsersClient, get_private_users_client
 from clients.users.public_users_client import get_public_users_client, PublicUsersClient
@@ -22,11 +21,6 @@ class UserFixture(BaseModel):
     @property
     def authentication_user(self) -> AuthenticationUserSchema:
         return AuthenticationUserSchema(email=self.email, password=self.password)
-
-
-@pytest.fixture
-def authentication_client() -> AuthenticationClient:
-    return get_authentication_client()
 
 
 @pytest.fixture
