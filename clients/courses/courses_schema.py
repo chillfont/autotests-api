6b857dc76032a56bdf398f80504c45d4f6fc39c2
chillfont <chillfont@gmail.com>
2026-08-20 00:16:59 +0700

    9.3 Практикуемся в написании API автотестов. Часть 2

--- clients/courses/courses_schema.py
+++ clients/courses/courses_schema.py
@@ -5,7 +5,7 @@ from clients.users.users_schema import UserSchema
 from tools.fakers import fake
 
 
-class Course(BaseModel):
+class CourseSchema(BaseModel):
     """
     Описание структуры курса.
     """
@@ -30,6 +30,13 @@ class GetCoursesQuerySchema(BaseModel):
     user_id: str = Field(alias="userId")
 
 
+class GetCoursesResponseSchema(BaseModel):
+    """
+    Описание структуры ответа на получение списка курсов.
+    """
+    courses: list[CourseSchema]
+
+
 class CreateCourseRequestSchema(BaseModel):
     """
     Описание структуры запроса на создание курса.
@@ -49,7 +56,7 @@ class CreateCourseResponseSchema(BaseModel):
     """
     Описание структуры ответа создания курса.
     """
-    course: Course
+    course: CourseSchema
 
 
 class UpdateCourseRequestSchema(BaseModel):
@@ -63,3 +70,10 @@ class UpdateCourseRequestSchema(BaseModel):
     min_score: int | None = Field(alias="minScore", default_factory=fake.min_score)
     description: str | None = Field(default_factory=fake.text)
     estimated_time: str | None = Field(alias="estimatedTime", default_factory=fake.estimated_time)
+
+
+class UpdateCourseResponseSchema(BaseModel):
+    """
+    Описание структуры ответа обновления курса.
+    """
+    course: CourseSchema
